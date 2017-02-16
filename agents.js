ce8c5bff728f99ecaacf0d4b1867921df1a4f47a
Gasnier Maxime <maxime.gasnier.1@gmail.com>
2017-02-16 23:31:10 +0100

    Refacto agent.js

--- agents.js
+++ agents.js
@@ -1,28 +1,22 @@
-let request = require('request');
-let cheerio = require('cheerio');
-let modLeboncoin = require('./leboncoin')
+var request = require('request'),
+    cheerio = require('cheerio'),
+    modLeboncoin = require('./leboncoin');
+
+exports.scrap = (url, callback) => {
 
 
-;
-
-exports.scrap = (url,callback) =>{
-
-
-	request(url,(er,resp,body) =>{
-	let $ = cheerio.load(body);
-
-	let avPriceFlat = $('.small-4.medium-2.columns.prices-summary__cell--median').first().text()
-	let $avPriceFlat = Number(avPriceFlat.replace(/[^0-9\.]+/g,""));
-
-	let avPriceHouse =  $('.small-4.medium-2.columns.prices-summary__cell--median').eq(1).text()
-	let $avPriceHouse =  Number(avPriceHouse.replace(/[^0-9\.]+/g,""));
+	request(url, (er,resp,body) => {
+	let $             = cheerio.load(body),
+	    avPriceFlat   = $('.small-4.medium-2.columns.prices-summary__cell--median').first().text(),
+	    $avPriceFlat  = Number(avPriceFlat.replace(/[^0-9\.]+/g,"")),
+	    avPriceHouse  =  $('.small-4.medium-2.columns.prices-summary__cell--median').eq(1).text(),
+	    $avPriceHouse =  Number(avPriceHouse.replace(/[^0-9\.]+/g,""));
 
 	let agents = {
 			flat  : $avPriceFlat,
 			house  : $avPriceHouse,
 			}
 			callback(agents)
-
 	})
 
 
